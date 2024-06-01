8d9cc23c0a09fca09298b960a3af90420007ccc2
Samirkhairati <samirk10101@gmail.com>
2024-06-01 09:35:54 +0530

    fix: color fixes

--- components/layout/navbar/search-bar.tsx
+++ components/layout/navbar/search-bar.tsx
@@ -20,14 +20,14 @@ const SearchBar = () => {
     return (
         <form onSubmit={handleSearch} className="ml-auto flex-1 sm:flex-initial">
             <div className="relative">
-                <SearchIcon className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                {/* <SearchIcon className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
                 <Input
                     value={search}
                     onChange={(e) => setSearch(e.target.value)}
                     className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
                     placeholder="Search products..."
                     type="search"
-                />
+                /> */}
             </div>
         </form>
     )
